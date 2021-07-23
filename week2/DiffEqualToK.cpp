f7450f44afc2a3f4af6b032712e382f7be6ba40f
Govind Singh <76403417+Govind-singh4822@users.noreply.github.com>
2021-07-23 22:53:26 +0530

    Add files via upload

---
+++ week2/DiffEqualToK.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        int arr[n];
+
+        for(int i=1;i<=n;i++)
+        {
+            cin>>arr[i];
+        }
+
+        int k,count=0;
+        cin>>k;
+
+       for(int i=1;i<=n;i++)
+       {
+           for(int j=n;j>=1;j--)
+           {
+               if((arr[i]-arr[j])==k)
+               {
+                   count++;
+               }
+           }
+       }
+       cout<<count<<endl;
+        
+    }
+}
